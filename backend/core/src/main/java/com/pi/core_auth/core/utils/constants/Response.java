a9d2548bdd88fd0feebe5fb50646078a35623930
GustavoBoaz <gustavo.boaz@hotmail.com>
2025-04-18 17:58:55 -0300

    Gustavo.Boaz/OTHER/UPDATE-context security

--- backend/core/src/main/java/com/pi/core_auth/core/utils/constants/Response.java
+++ backend/core/src/main/java/com/pi/core_auth/core/utils/constants/Response.java
@@ -8,18 +8,38 @@ import java.util.EnumSet;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
+    /**
+     * EnumSet representing the scope of the response.
+     */
     public EnumSet<ScopeType> scope;
+
+    /**
+     * Enum representing the status of the response.
+     */
     public StatusType status;
 
-    public Response(Builder builder) {
+    /**
+     * Constructor for the Response class.
+     *
+     * @param builder The builder instance used to create the response.
+     */
+    private Response(Builder builder) {
         this.scope = builder.scope;
         this.status = builder.status;
     }
 
+    /**
+     * Static method to create a new Builder instance.
+     *
+     * @return A new Builder instance.
+     */
     public static Builder builder() {
         return new Builder();
     }
 
+    /**
+     * Static class to create a new Response instance using the Builder pattern.
+     */
     public static class Builder {
         private EnumSet<ScopeType> scope;
         private StatusType status;
